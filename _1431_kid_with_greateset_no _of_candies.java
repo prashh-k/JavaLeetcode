d1bbcc348783be8396cfe561d00e4c263cb6ad98
prashantkharche <prashantkharche2002@gmail.com>
2023-12-10 21:19:58 +0530

    candies

---
+++ _1431_kid_with_greateset_no _of_candies.java
@@ -0,0 +1,24 @@
+class Solution {
+    public List<Boolean> kidsWithCandies(int[] candies, int extraCandies) {
+       ArrayList <Boolean> arr = new ArrayList<>();   //arraylist declaration
+    int max = candies[0];    //finding max element in the given array
+    for(int i=0;i<candies.length;i++){
+        if(candies[i] > max){
+            max = candies[i];
+        }
+    }
+	//adding extras and check max or not and add list true or false accordingly
+    for(int i=0;i<candies.length;i++){ 
+        if((candies[i]+extraCandies)>= max){
+            arr.add(true);
+        }
+        else{
+            arr.add(false);
+        }
+    }
+   // System.out.println(arr);
+    return arr;
+    
+    
+}
+}
